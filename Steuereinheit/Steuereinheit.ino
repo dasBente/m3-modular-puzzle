81213d2b111bcc001abc0f7eef80f3c0ce0d701d
Tobias Möhring <dasbente@gmail.com>
2016-06-10 20:51:09 +0200

    Hauptprogrammdatei für Steuereinheit erstellt
    Steuerprogramm für die Steuereinheit erstellt und Verschaltung von
    Mikrocontroller und LCD vorerst in die .ino eingetragen.

---
+++ Steuereinheit/Steuereinheit.ino
@@ -0,0 +1,60 @@
+/*
+  Hauptprogramm der Steuereinheit
+  
+Umfasst Ansteuerung eines LCDs (kompatibel mit HD44780 Treiber. Z.B. Qapass 1602A).
+
+===============================================
+| PIN Belegung des Micro Kontrollers          |     
+===============================================
+ mC Pin |  Verbinden mit
+-----------------------------------------------
+  P1.0  | ---
+  P1.1  | ---
+  P1.2  | ---
+  P1.3  | ---
+  P1.4  | --- 
+  P1.5  | ---
+  P1.6  | ---
+  P1.7  | ---
+  P2.0  | RS (Register Select) (LCD)
+  P2.1  | E  (Enable)          (LCD)
+  P2.2  | D4                   (LCD)
+  P2.3  | D5                   (LCD)
+  P2.4  | D6                   (LCD)
+  P2.5  | D7                   (LCD)
+===============================================
+
+===============================================
+| Schaltung LCD (für HD44780)                 |
+===============================================
+ LCD Pin    | Verbinden mit
+ ==============================================
+  P1  (VSS) | GND
+  P2  (VDD) | VCC
+  P3  (V0)  | mittlerer Pin von Potentiometer (andere Pins: GND und VCC)
+  P4  (RS)  | P2.0
+  P5  (RW)  | GND
+  P6  (E)   | P2.1
+  P7  (D0)  | ---
+  P8  (D1)  | ---
+  P9  (D2)  | ---
+  P10 (D3)  | --- 
+  P11 (D4)  | P2.2
+  P12 (D5)  | P2.3
+  P13 (D6)  | P2.4
+  P14 (D7)  | P2.5
+  P15 (A)   | mit 220 Ohm an VCC
+  P16 (K)   | GND
+*/
+
+void setup()
+{
+  // put your setup code here, to run once:
+  
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  
+}
